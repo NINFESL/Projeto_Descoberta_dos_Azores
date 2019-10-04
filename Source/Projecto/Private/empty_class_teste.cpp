7c33f7360fa611c26496d8e88f081b7ea07afe3a
kuflah <39557917+kuflah@users.noreply.github.com>
2019-10-04 12:50:19 +0000

    c++ update files

---
+++ Source/Projecto/Private/empty_class_teste.cpp
@@ -0,0 +1,12 @@
+?// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "empty_class_teste.h"
+
+empty_class_teste::empty_class_teste()
+{
+}
+
+empty_class_teste::~empty_class_teste()
+{
+}
